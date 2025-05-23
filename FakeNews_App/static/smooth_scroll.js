14b20901ae2a2cf5b79c16bf86b19d9113bded60
arobert004 <anthony.robert.pro1@gmail.com>
2025-05-23 11:06:04 +0200

    nettoyage index.html et sépartion de la partie js

---
+++ FakeNews_App/static/smooth_scroll.js
@@ -0,0 +1,18 @@
+// Toggle navigation menu
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    navToggle.classList.toggle('active');
+});
+
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
